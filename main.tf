f60bf79401b7bbdfdfe59310262d88bea5f4b397
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2019-10-22 16:18:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_security_group" "private_network_access" {
-  name        = "private_network_access1"
+  name        = "private_network_access2"
   description = "security group for private network access"
   vpc_id      = "${data.aws_vpc.default.id}"
 
